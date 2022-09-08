2135535b6e889947a55d5a5c2e05e3ebedc23b36
Luis Ernesto Hernandez Rivera <ernes@DESKTOP-D1ICO8M>
2022-09-07 22:07:06 -0500

    se guardan las propiedades con mvc

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,20 +3,79 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use Model\Vendedor;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PropiedadController {
   public static function index(Router $router) {
     $propiedades = Propiedad::all();
     $router->render('propiedades/Admin', [
-      'propiedades' => $propiedades
+      'propiedades' => $propiedades,
+      'resultado' => $_GET['resultado'] ?? null
     ]);
   }
 
-  public static function crear() {
-    echo "crear";
+  public static function crear(Router $router) {
+    $propiedad = new Propiedad;
+    $vendedores = Vendedor::all();
+    $errores = Propiedad::getErrores();
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $propiedad = new Propiedad($_POST['propiedad']);
+
+      $nombreImagen = md5(uniqid(rand(), true));
+      if($_FILES['propiedad']['tmp_name']['imagen']){
+        $imagen = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+        $propiedad->setImagen($nombreImagen);
+      }
+
+      $errores = $propiedad->validar();
+      if(empty($errores)){
+        if(!is_dir(CARPETA_IMAGENES)){
+          mkdir(CARPETA_IMAGENES);
+        }
+
+        $imagen->save(CARPETA_IMAGENES . $nombreImagen . ".jpg");
+        
+        $resultado = $propiedad->guardar();
+
+        if($resultado){
+          header('Location: /admin?resultado=1');
+        } else {
+          echo 'Error al insertar';
+        }
+      }
+    }
+    $router->render('propiedades/Crear', [
+      'propiedad' => $propiedad,
+      'vendedores' => $vendedores,
+      'errores' => $errores
+    ]);
   }
 
-  public static function actualizar() {
-    echo "actualizar";
+  public static function actualizar(Router $router) {
+    $id = validarORedireccionar('/bienesraices/admin');
+    $propiedad = Propiedad::find($id);
+    $errores = Propiedad::getErrores();
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $args = $_POST['propiedad'];
+      $propiedad->sincronizar($args);
+      $errores = $propiedad->validar();
+      if(empty($errores)) {
+        $carpetaImagenes = __DIR__ . "/../imagenes/";
+        if(!is_dir($carpetaImagenes)) {
+          mkdir($carpetaImagenes);
+        }
+        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+        // Subida de archivos
+        move_uploaded_file($_FILES['propiedad']['tmp_name']['imagen'], $carpetaImagenes . $nombreImagen);
+        $propiedad->setImagen($nombreImagen);
+        $propiedad->guardar();
+        header('Location: /bienesraices/admin?resultado=2');
+      }
+    }
+    $router->render('propiedades/Actualizar', [
+      'propiedad' => $propiedad,
+      'errores' => $errores
+    ]);
   }
 }
--- views/propiedades/Admin.php
+++ views/propiedades/Admin.php
@@ -1,2 +1,46 @@
-<?php
-echo "Hola mundo";
+<main class="contenedor seccion">
+  <h1>Administrador de Bienes Raices</h1>
+  <?php 
+  if($resultado) {
+    $mensaje = mostrarNotificacion(intval($resultado));
+    if($mensaje): ?>
+      <p class="alerta exito"><?php echo eliminaHTML($mensaje); ?></p>
+    <?php endif;
+  } ?>
+
+  <a href="/propiedades/crear" class="boton boton-verde">Nueva Propiedad</a>
+  <a href="/bienesraices/admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
+  <h2>Propiedades</h2>
+  <table class="propiedades">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Titulo</th>
+        <th>Imagen</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($propiedades as $propiedad): ?>
+      <tr>
+        <td><?php echo $propiedad->id; ?></td>
+        <td><?php echo $propiedad->titulo; ?></td>
+        <td><img class="imagen-tabla" src=<?php echo "../imagenes/" . $propiedad->imagen . ".jpg";?>
+            alt="Casa en el centro de la ciudad"></td>
+        <td><?php echo "$" . $propiedad->precio; ?></td>
+        <td>
+          <a href="/bienesraices/admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>"
+            class="boton-amarillo-block">Actualizar</a>
+          <form method="POST" class="w-100">
+            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+            <input type="hidden" name="tipo" value="propiedad">
+            <input type="submit" class="boton-rojo-block boton" value="Eliminar">
+          </form>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+</main>
